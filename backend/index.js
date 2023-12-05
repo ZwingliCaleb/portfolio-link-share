28673f2251eb6fd325e9fa51a7a8ff4d818a0c9f
ZwingliCaleb <calebzwingli@gmail.com>
2023-12-05 13:43:17 +0300

    add new backend directory and new git ignore file

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the backend!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
