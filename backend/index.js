6455c65b88886b551cbb85b30ce443885dc563af
ZwingliCaleb <calebzwingli@gmail.com>
2023-12-05 16:27:27 +0300

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,42 @@
+// Import necessary modules
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const cors = require('cors');
-require('dotenv').config();
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an instance of Express
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
+// MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('MongoDB database connection established successfully');
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
 });
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Welcome to the backend!');
-});
+const authRouter = require('./routes/auth');
+const linksRouter = require('./routes/links');
+
+app.use('/auth', authRouter);
+app.use('/links', linksRouter);
 
 // Start the server
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
